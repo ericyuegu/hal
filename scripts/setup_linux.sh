a434b20cffb43872ace048d31305a642a0980fcc
Eric Gu <eric.yue.gu@gmail.com>
2024-06-18 23:04:25 -0700

    Install .7z & unzip iso in setup

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -23,5 +23,11 @@ echo "${Yellow}Cloned hal2 repo and installed requirements"
 
 EMULATOR_DIR="/Users/ericgu/data/SSBM"
 REMOTE_EMULATOR_DIR="/opt/slippi"
+EMULATOR_FILE_NAME="ssbm_ntsc_1.02.7z"
 rsync -avz --delete --filter=":- .gitignore" --exclude=".DS_Store" --exclude=".localized" -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR" $EMULATOR_DIR/ $notebook:$REMOTE_EMULATOR_DIR/
+ssh $notebook /bin/bash << EOF
+  sudo apt-get install p7zip-full
+  cd $REMOTE_EMULATOR_DIR
+  7za x $EMULATOR_FILE_NAME
+EOF
 echo "${Yellow}Synced emulator & iso"
