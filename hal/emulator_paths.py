82b2242954a6d97e32d7baa735e5cb0a9a1522c1
Eric Gu <eric.yue.gu@gmail.com>
2024-06-30 12:58:02 -0700

    Adding local and remote emulator paths

--- hal/emulator_paths.py
+++ hal/emulator_paths.py
@@ -1,4 +1,7 @@
 from typing import Final
 
-REMOTE_EMULATOR_PATH: Final[str] = "/home/egu/.config/Slippi Launcher/netplay/Slippi_Online-x86_64.AppImage"
-REMOTE_CISO_PATH: Final[str] = "/home/egu/data/ssbm/ssbm.ciso"
+REMOTE_EMULATOR_PATH: Final[str] = "/opt/slippi/Slippi_Online-Ubuntu20.04-Exi-x86_64.AppImage"
+REMOTE_CISO_PATH: Final[str] = "/opt/slippi/ssbm.ciso"
+LOCAL_GUI_EMULATOR_PATH: Final[str] = "/home/egu/data/ssbm/gui/Slippi_Online-x86_64.AppImage"
+LOCAL_HEADLESS_EMULATOR_PATH: Final[str] = "/home/egu/src/hal2/emulator/Slippi_Online-Ubuntu20.04-Exi-x86_64.AppImage"
+LOCAL_CISO_PATH: Final[str] = "/home/egu/data/ssbm/ssbm.ciso"
