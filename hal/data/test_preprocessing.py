6147c0f83fe7c3af742c77c257c4113f69f7704b
Eric Gu <eric.yue.gu@gmail.com>
2024-07-05 23:17:27 -0700

    Add test case

--- hal/data/test_preprocessing.py
+++ hal/data/test_preprocessing.py
@@ -6,6 +6,52 @@ from hal.data.preprocessing import one_hot_3d
 
 
 def test_convert_target_to_one_hot_3d() -> None:
+    # Test case 0
+    arr0 = np.array(
+        [
+            [
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 1, 0],
+                [0, 0, 0, 1, 1, 0],
+                [0, 0, 0, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+            ]
+        ],
+        dtype=np.int8,
+    )
+    expected0 = np.array(
+        [
+            [
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 1],
+            ]
+        ],
+        dtype=np.int8,
+    )
+    # assert_array_equal(arr1, expected1, err_msg=f"{arr1}\n{expected1}")
+    assert_array_equal(one_hot_3d(arr0), expected0, err_msg=f"{one_hot_3d(arr0)}\n{expected0}")
+
     # Test case 1: Basic scenario (keep the same as before)
     arr1 = np.array(
         [
