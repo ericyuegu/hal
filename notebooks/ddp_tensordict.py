cc951e780e8767d0a41e05d32fec115fe0b73aca
Eric Gu <eric.yue.gu@gmail.com>
2024-09-01 15:49:42 -0700

    It working

--- notebooks/ddp_tensordict.py
+++ notebooks/ddp_tensordict.py
@@ -15,6 +15,8 @@ from torch.utils.data.distributed import DistributedSampler
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+from hal.training.distributed import is_master
+
 
 def get_device() -> str:
     return "cuda" if torch.cuda.is_available() else "cpu"
@@ -89,15 +91,31 @@ def load_data() -> tuple[TensorDict, TensorDict]:
     for i, (img, label) in enumerate(test_data):
         test_data_td[i] = TensorDict({"images": img, "targets": label}, [])
 
+    print("Checking if tensors are already in shared memory:")
+    print(f"Training images: {training_data_td['images'].is_shared()}")
+    print(f"Training targets: {training_data_td['targets'].is_shared()}")
+    print(f"Test images: {test_data_td['images'].is_shared()}")
+    print(f"Test targets: {test_data_td['targets'].is_shared()}")
+
+    print("Sharing tensors...")
+    training_data_td["images"].share_memory_()
+    training_data_td["targets"].share_memory_()
+    test_data_td["images"].share_memory_()
+    test_data_td["targets"].share_memory_()
+
     return training_data_td, test_data_td
 
 
 def train_ddp(rank, world_size, training_data_td: TensorDict, test_data_td: TensorDict) -> None:
     setup(rank, world_size)
-    device = get_device()
+    device = torch.device(f"cuda:{rank}")  # Use the GPU corresponding to the rank
     print(f"{rank=}, Mem address of training_data_td: {hex(id(training_data_td))}")
     for k, v in training_data_td.items():
-        print(f"{rank=}, Mem address of {k}: {hex(id(v))}")
+        # print(f"{rank=}, Mem address of {k}: {hex(id(v))}")
+        # Increment last slice by rank and save it back
+        last_slice = v[-1].clone()
+        v[-1] = last_slice + rank
+        print(f"{rank=}, Incremented last slice of {k}: {last_slice} -> {v[-1]}")
 
     model = Net().to(device)
     model = DDP(model, device_ids=[rank])
@@ -112,8 +130,8 @@ def train_ddp(rank, world_size, training_data_td: TensorDict, test_data_td: Tens
     print(f"{rank=} Initialized dataloader")
 
     # Training loop
-    epochs = 5
-    for epoch in range(epochs):
+    epochs = 1
+    for _ in range(epochs):
         model.train()
         for batch, data in enumerate(train_dataloader):
             X, y = data["images"].contiguous(), data["targets"].contiguous()
@@ -127,7 +145,8 @@ def train_ddp(rank, world_size, training_data_td: TensorDict, test_data_td: Tens
             optimizer.step()
 
             current = batch * len(X)
-            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+            if is_master():
+                print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
 
     cleanup()
 
@@ -136,7 +155,9 @@ if __name__ == "__main__":
     world_size = torch.cuda.device_count()
 
     # Load data into shared memory
+    print(f"Loading data")
     training_data_td, test_data_td = load_data()
 
     # Use mp.spawn to launch the training process on each GPU
+    print(f"Spawning workers")
     mp.spawn(train_ddp, args=(world_size, training_data_td, test_data_td), nprocs=world_size, join=True)
