750d8b4dd6e5798253c869729742f0a73de86bb4
Eric Gu <eric.yue.gu@gmail.com>
2024-09-19 07:00:32 -0700

    Overfit on single batch multi head multi frame

--- notebooks/debug_loss.py
+++ notebooks/debug_loss.py
@@ -54,3 +54,50 @@ for i in range(1000):
     print(loss.item())
     loss.backward()
     optimizer.step()
+
+# %%
+# overfit on single example multiple heads
+model, train_config = load_model_from_artifact_dir(artifact_dir)
+model.train()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+criterion = torch.nn.CrossEntropyLoss()
+heads = ["buttons", "main_stick", "c_stick"]
+
+for i in range(1000):
+    optimizer.zero_grad()
+    y_hat = model(x)
+    total_loss = 0
+    for head in heads:
+        loss = criterion(y_hat[head], y[head].squeeze(-2))
+        total_loss += loss
+    print(total_loss.item())
+    total_loss.backward()
+    optimizer.step()
+
+# %%
+# overfit on single example multiple heads multiple frames
+model, train_config = load_model_from_artifact_dir(artifact_dir)
+model.train()
+input_len = train_config.data.input_len
+target_len = train_config.data.target_len
+batch = TensorDict.load(artifact_dir / "training_samples" / "0")
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+criterion = torch.nn.CrossEntropyLoss()
+heads = ["buttons", "main_stick", "c_stick"]
+frames = 5
+
+for i in range(1000):
+    optimizer.zero_grad()
+    total_loss = 0
+
+    for t in range(frames):
+        x_t = batch["inputs"][:, t : t + input_len]
+        y_t = batch["targets"][:, t + input_len : t + input_len + 1]
+        for head in heads:
+            y_hat = model(x_t)
+            loss = criterion(y_hat[head], y_t[head].squeeze(-2))
+            total_loss += loss
+    print(total_loss.item())
+    total_loss.backward()
+    optimizer.step()
