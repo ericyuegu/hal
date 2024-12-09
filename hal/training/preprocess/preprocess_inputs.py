9032737620305705b2dc4212c7c2d1d81d97b627
Eric Gu <eric.yue.gu@gmail.com>
2024-12-08 16:17:26 -0800

    WIP refactor preprocessing pt5
    - Consolidate preprocessing logic to single file
    - Delete deprecated input preprocessing code

--- hal/training/preprocess/preprocess_inputs.py
+++ hal/training/preprocess/preprocess_inputs.py
@@ -1,119 +1,166 @@
-from functools import partial
 from typing import Dict
+from typing import Set
+from typing import Tuple
 
+import attr
+import torch
 from tensordict import TensorDict
+from training.preprocess.registry import InputPreprocessRegistry
 
-from hal.constants import PLAYER_INPUT_FEATURES_TO_EMBED
-from hal.constants import PLAYER_INPUT_FEATURES_TO_INVERT_AND_NORMALIZE
-from hal.constants import PLAYER_INPUT_FEATURES_TO_NORMALIZE
-from hal.constants import PLAYER_POSITION
 from hal.constants import Player
-from hal.constants import STAGE
-from hal.constants import STICK_XY_CLUSTER_CENTERS_V0
-from hal.constants import TARGET_FEATURES_TO_ONE_HOT_ENCODE
+from hal.constants import get_opponent
 from hal.data.stats import FeatureStats
-from hal.training.config import DataConfig
-from hal.training.preprocess.config import InputPreprocessConfig
-from hal.training.preprocess.preprocess_targets import preprocess_targets_v0
-from hal.training.preprocess.registry import InputPreprocessRegistry
+from hal.training.config import EmbeddingConfig
 from hal.training.preprocess.transform import Transformation
 from hal.training.preprocess.transform import cast_int32
 from hal.training.preprocess.transform import invert_and_normalize
 from hal.training.preprocess.transform import normalize
-from hal.training.preprocess.transform import normalize_and_embed_fourier
 from hal.training.preprocess.transform import standardize
 
 
-@InputPreprocessRegistry.register("inputs_v0", InputPreprocessConfig.v0())
-def preprocess_inputs_v0(
-    sample: TensorDict, data_config: DataConfig, ego: Player, stats: Dict[str, FeatureStats]
-) -> TensorDict:
-    """Slice input sample to the input length.
+@attr.s(auto_attribs=True)
+class InputPreprocessConfig:
+    """Configuration for preprocessing functions."""
 
-    Expects tensordict with shape (trajectory_len,)
+    # Features to preprocess twice, once for each player
+    player_features: Tuple[str, ...]
+
+    # Mapping from feature name to normalization function
+    normalization_fn_by_feature_name: Dict[str, Transformation]
+
+    # Mapping from feature name to frame offset relative to sampled index
+    # e.g. to include controller inputs from prev frame with current frame gamestate, set p1_button_a = -1, etc.
+    frame_offsets_by_feature: Dict[str, int]
+
+    # Mapping from head name to features to be fed to that head
+    # Usually for int categorical features
+    # All unlisted features are concatenated to the default "gamestate" head
+    grouped_feature_names_by_head: Dict[str, Tuple[str, ...]]
+
+    # Input dimensions (D,) of concatenated features after preprocessing
+    # TensorDict does not support differentiated sizes across keys for the same dimension
+    input_shapes_by_head: Dict[str, Tuple[int, ...]]
+
+    # TODO what if we want to add or remove heads?
+    def update_input_shapes_with_embedding_config(
+        self, embedding_config: EmbeddingConfig
+    ) -> Dict[str, Tuple[int, ...]]:
+        new_input_shapes_by_head = self.input_shapes_by_head.copy()
+        new_input_shapes_by_head.update(
+            {
+                "stage": (embedding_config.stage_embedding_dim,),
+                "ego_character": (embedding_config.character_embedding_dim,),
+                "opponent_character": (embedding_config.character_embedding_dim,),
+                "ego_action": (embedding_config.action_embedding_dim,),
+                "opponent_action": (embedding_config.action_embedding_dim,),
+            }
+        )
+        self.input_shapes_by_head = new_input_shapes_by_head
+        return new_input_shapes_by_head
+
+    @classmethod
+    def v0(cls):
+        """
+        Baseline input features.
+
+        Separate embedding heads for stage, character, & action.
+        No controller, no platforms, no projectiles.
+        """
+
+        player_features = (
+            "character",
+            "action",
+            "percent",
+            "stock",
+            "facing",
+            "invulnerable",
+            "jumps_left",
+            "on_ground",
+            "shield_strength",
+            "position_x",
+            "position_y",
+        )
+
+        return cls(
+            player_features=player_features,
+            normalization_fn_by_feature_name={
+                "frame": cast_int32,
+                "stage": cast_int32,
+                "character": cast_int32,
+                "action": cast_int32,
+                "percent": normalize,
+                "stock": normalize,
+                "facing": normalize,
+                "invulnerable": normalize,
+                "jumps_left": normalize,
+                "on_ground": normalize,
+                "shield_strength": invert_and_normalize,
+                "position_x": standardize,
+                "position_y": standardize,
+            },
+            frame_offsets_by_feature={},
+            grouped_feature_names_by_head={
+                "stage": ("stage",),
+                "ego_character": ("ego_character",),
+                "opponent_character": ("opponent_character",),
+                "ego_action": ("ego_action",),
+                "opponent_action": ("opponent_action",),
+            },
+            input_shapes_by_head={
+                "gamestate": (2 * len(player_features),),  # 2x for ego and opponent
+            },
+        )
+
+
+### Register configs here
+InputPreprocessRegistry.register("inputs_v0", InputPreprocessConfig.v0())
+
+
+def preprocess_input_features(
+    sample: TensorDict,
+    ego: Player,
+    config: InputPreprocessConfig,
+    stats: Dict[str, FeatureStats],
+) -> TensorDict:
+    """Applies preprocessing functions to player and non-player input features for a given sample.
+
+    Does not slice or shift any features.
     """
-    trajectory_len = data_config.input_len + data_config.target_len
+    opponent = get_opponent(ego)
+    normalization_fn_by_feature_name = config.normalization_fn_by_feature_name
+    processed_features: Dict[str, torch.Tensor] = {}
 
-    return TensorDict(
-        preprocess_input_features(
-            sample=sample[:trajectory_len],
-            ego=ego,
-            config=InputPreprocessConfig.v0(),
-            stats=stats,
-        ),
-        batch_size=(trajectory_len,),
-    )
+    # Process player features
+    for player in (ego, opponent):
+        perspective = "ego" if player == ego else "opponent"
+        for feature_name in config.player_features:
+            preprocess_fn = normalization_fn_by_feature_name[feature_name]
+            player_feature_name = f"{perspective}_{feature_name}"
+            processed_features[player_feature_name] = preprocess_fn(
+                sample[player_feature_name], stats[player_feature_name]
+            )
 
+    # Process non-player features
+    non_player_features = [
+        feature_name for feature_name in normalization_fn_by_feature_name if feature_name not in config.player_features
+    ]
+    for feature_name in non_player_features:
+        processed_features[feature_name] = preprocess_fn(sample[feature_name], stats[feature_name])
 
-NUMERIC_FEATURES_V1 = tuple(
-    PLAYER_INPUT_FEATURES_TO_NORMALIZE + PLAYER_INPUT_FEATURES_TO_INVERT_AND_NORMALIZE + PLAYER_POSITION
-)
+    # Concatenate processed features by head
+    concatenated_features_by_head_name: Dict[str, torch.Tensor] = {}
+    seen_feature_names: Set[str] = set()
+    for head_name, feature_names in config.grouped_feature_names_by_head.items():
+        features_to_concatenate = [processed_features[feature_name] for feature_name in feature_names]
+        concatenated_features_by_head_name[head_name] = torch.cat(features_to_concatenate, dim=-1)
+        seen_feature_names.update(feature_names)
 
+    # Add features that are not associated with any head to default `gamestate` head
+    DEFAULT_HEAD_NAME = "gamestate"
+    unseen_feature_tensors = []
+    for feature_name, feature_tensor in processed_features.items():
+        if feature_name not in seen_feature_names:
+            unseen_feature_tensors.append(feature_tensor)
+    concatenated_features_by_head_name[DEFAULT_HEAD_NAME] = torch.cat(unseen_feature_tensors, dim=-1)
 
-# extra input dimensions from Fourier embedding
-@InputPreprocessRegistry.register("inputs_v1", num_features=2 * (len(NUMERIC_FEATURES_V1) + 7 * len(PLAYER_POSITION)))
-def preprocess_inputs_v1(
-    sample: TensorDict, data_config: DataConfig, ego: Player, stats: Dict[str, FeatureStats]
-) -> TensorDict:
-    """Slice input sample to the input length."""
-    trajectory_len = data_config.input_len + data_config.target_len
-
-    numeric_features = NUMERIC_FEATURES_V1
-    normalization_fn_by_feature_name: Dict[str, Transformation] = {
-        **dict.fromkeys(STAGE, cast_int32),
-        **dict.fromkeys(PLAYER_INPUT_FEATURES_TO_EMBED, cast_int32),
-        **dict.fromkeys(PLAYER_INPUT_FEATURES_TO_NORMALIZE, normalize),
-        **dict.fromkeys(PLAYER_INPUT_FEATURES_TO_INVERT_AND_NORMALIZE, invert_and_normalize),
-        **dict.fromkeys(PLAYER_POSITION, partial(normalize_and_embed_fourier, dim=8)),
-    }
-
-    return _preprocess_features_by_mapping(
-        sample=sample,
-        ego=ego,
-        stats=stats,
-        player_numeric_feature_names=numeric_features,
-        normalization_fn_by_feature_name=normalization_fn_by_feature_name,
-        batch_size=(trajectory_len,),
-    )
-
-
-@InputPreprocessRegistry.register(
-    "inputs_v2",
-    num_features=2 * (len(PLAYER_NUMERIC_FEATURES_V0) + len(STICK_XY_CLUSTER_CENTERS_V0))
-    + len(TARGET_FEATURES_TO_ONE_HOT_ENCODE),
-)
-def preprocess_inputs_v2(
-    sample: TensorDict, data_config: DataConfig, ego: Player, stats: Dict[str, FeatureStats]
-) -> TensorDict:
-    player_numeric_feature_names = PLAYER_NUMERIC_FEATURES_V0
-    normalization_fn_by_feature_name: Dict[str, Transformation] = {
-        **dict.fromkeys(STAGE, cast_int32),
-        **dict.fromkeys(PLAYER_INPUT_FEATURES_TO_EMBED, cast_int32),
-        **dict.fromkeys(PLAYER_INPUT_FEATURES_TO_NORMALIZE, normalize),
-        **dict.fromkeys(PLAYER_INPUT_FEATURES_TO_INVERT_AND_NORMALIZE, invert_and_normalize),
-        **dict.fromkeys(PLAYER_POSITION, standardize),
-    }
-
-    # check if sequence length is >1
-    if sample.shape[-1] > 1:
-        preprocessed_inputs = _preprocess_features_by_mapping(
-            sample=sample[1 : data_config.input_len + 1],
-            ego=ego,
-            stats=stats,
-            player_numeric_feature_names=player_numeric_feature_names,
-            normalization_fn_by_feature_name=normalization_fn_by_feature_name,
-            batch_size=(data_config.input_len,),
-        )
-        ego_controller = preprocess_targets_v0(sample=sample[0 : data_config.input_len], player=ego)
-        preprocessed_inputs.update(ego_controller)
-    else:
-        preprocessed_inputs = _preprocess_features_by_mapping(
-            sample=sample,
-            ego=ego,
-            stats=stats,
-            player_numeric_feature_names=player_numeric_feature_names,
-            normalization_fn_by_feature_name=normalization_fn_by_feature_name,
-            batch_size=(data_config.input_len,),
-        )
-
-    return preprocessed_inputs
+    return TensorDict(concatenated_features_by_head_name, batch_size=sample.batch_size)
--- hal/training/preprocess/preprocessor.py
+++ hal/training/preprocess/preprocessor.py
@@ -1,71 +1,17 @@
 import random
 from typing import Dict
-from typing import Set
 
 import numpy as np
 import torch
 from tensordict import TensorDict
 
 from hal.constants import Player
-from hal.constants import get_opponent
-from hal.data.stats import FeatureStats
 from hal.data.stats import load_dataset_stats
 from hal.training.config import DataConfig
 from hal.training.config import EmbeddingConfig
-from hal.training.preprocess.config import InputPreprocessConfig
-from hal.training.preprocess.config import update_input_shapes_with_embedding_config
+from hal.training.preprocess.preprocess_inputs import preprocess_input_features
 from hal.training.preprocess.registry import InputPreprocessRegistry
 from hal.training.preprocess.transform import Transformation
-
-
-def preprocess_input_features(
-    sample: TensorDict,
-    ego: Player,
-    config: InputPreprocessConfig,
-    stats: Dict[str, FeatureStats],
-) -> TensorDict:
-    """Applies preprocessing functions to player and non-player input features for a given sample.
-
-    Does not slice or shift any features.
-    """
-    opponent = get_opponent(ego)
-    normalization_fn_by_feature_name = config.normalization_fn_by_feature_name
-    processed_features: Dict[str, torch.Tensor] = {}
-
-    # Process player features
-    for player in (ego, opponent):
-        perspective = "ego" if player == ego else "opponent"
-        for feature_name in config.player_features:
-            preprocess_fn = normalization_fn_by_feature_name[feature_name]
-            player_feature_name = f"{perspective}_{feature_name}"
-            processed_features[player_feature_name] = preprocess_fn(
-                sample[player_feature_name], stats[player_feature_name]
-            )
-
-    # Process non-player features
-    non_player_features = [
-        feature_name for feature_name in normalization_fn_by_feature_name if feature_name not in config.player_features
-    ]
-    for feature_name in non_player_features:
-        processed_features[feature_name] = preprocess_fn(sample[feature_name], stats[feature_name])
-
-    # Concatenate processed features by head
-    concatenated_features_by_head_name: Dict[str, torch.Tensor] = {}
-    seen_feature_names: Set[str] = set()
-    for head_name, feature_names in config.grouped_feature_names_by_head.items():
-        features_to_concatenate = [processed_features[feature_name] for feature_name in feature_names]
-        concatenated_features_by_head_name[head_name] = torch.cat(features_to_concatenate, dim=-1)
-        seen_feature_names.update(feature_names)
-
-    # Add features that are not associated with any head to default `gamestate` head
-    DEFAULT_HEAD_NAME = "gamestate"
-    unseen_feature_tensors = []
-    for feature_name, feature_tensor in processed_features.items():
-        if feature_name not in seen_feature_names:
-            unseen_feature_tensors.append(feature_tensor)
-    concatenated_features_by_head_name[DEFAULT_HEAD_NAME] = torch.cat(unseen_feature_tensors, dim=-1)
-
-    return TensorDict(concatenated_features_by_head_name, batch_size=sample.batch_size)
 
 
 class Preprocessor:
@@ -87,8 +33,8 @@ class Preprocessor:
         self.seq_len = data_config.seq_len
 
         self.input_preprocess_config = InputPreprocessRegistry.get(self.embedding_config.input_preprocessing_fn)
-        self.input_shapes_by_head = update_input_shapes_with_embedding_config(
-            self.input_preprocess_config.input_shapes_by_head, self.embedding_config
+        self.input_shapes_by_head = self.input_preprocess_config.update_input_shapes_with_embedding_config(
+            self.embedding_config
         )
 
         self.frame_offsets_by_feature = self.input_preprocess_config.frame_offsets_by_feature
